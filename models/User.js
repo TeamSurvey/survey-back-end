7c2751022516953888cff886c4540e4c5616a0fa
Saad Rhoulam <saad@rhoulam.com>
2015-11-20 16:40:01 -0500

    Transform user model into a mongoose model.

--- models/User.js
+++ models/User.js
@@ -1,27 +1,47 @@
 'use strict';
-// export a function for defining a sequelize model
 
-module.exports = function(sequelize, DataTypes) {
-	var User = sequelize.define('User', {
-		id : {
-			type : DataTypes.INTEGER,
-			autoIncrement : true,
-			primaryKey : true
-		},
-		localName : {
-			type : DataTypes.STRING,
-			allowNull : false,
-			unique : true
-		},
-		localPass : {
-			type : DataTypes.STRING,
-			allowNull : false,
-			unique : false
-		}
-	}, {
-		timestamps : true
+var bcrypt = require('bcrypt');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// export a mongoose model
+
+var userSchema = new Schema({
+	userName : String,
+	passwordDigest : String
+});
+
+userSchema.virtual('password').set(function(password) {
+	var self = this;
+
+	var saltPromise = new Promise(function saltExec(res, rej) {
+		bcrypt.genSalt(16, function(err, salt) {
+			if(err) {
+				rej(err);
+				return;
+			}
+
+			res(salt);
+		});
 	});
 
-	return User;
-};
+	var returnedPromise = saltPromise.then(function(salt) {
+		return new Promise(function hashExec(res, rej) {
+			bcrypt.hash(password, salt, function(err, digest) {
+				if(err) {
+					rej(err);
+					return;
+				}
 
+				res(digest);
+			});
+		});
+	}).then(function(digest) {
+		self.passwordDigest = digest;
+	});
+
+	return returnedPromise;
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
