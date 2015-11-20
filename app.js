e5345c09287a039e80a3d760d08f78408dfe3eee
Saad Rhoulam <saad@rhoulam.com>
2015-11-20 17:44:17 -0500

    Remove mistake

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.json('error', {
+    res.json({
       message: err.message,
       error: err.stack
     });
@@ -72,7 +72,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.json('error', {
+  res.json({
     message: err.message,
     error: {}
   });
