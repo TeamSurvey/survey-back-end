66aefc41eb9b29cac38ccb5bfd5730040ee0ff9a
Ben <benjamin.a.spencer@gmail.com>
2015-11-30 12:34:10 -0500

    adds more templating to routes

--- routes/polls.js
+++ routes/polls.js
@@ -2,17 +2,17 @@
 
 var express = require('express');
 var router = express.Router();
-var pollCtrl = //reuqire file here//
+// var pollCtrl = require('../controllers/poll');
 
 //routing to an index action
 router.get('/', function(req, res, next) {
-  res.json({message: 'Hello, World!'});
+  Poll.find().exec();
   next();
 });
 
 //routing to a show action
 router.get('/:id', function(req, res, next) {
-  res.json({message: 'stuff'});
+  var poll_id = req.params.id;
   next();
 });
 
@@ -21,11 +21,16 @@ router.get('/', function(req, res) {
   res.status(200);
 });
 
-
-
-
 router.post('/', function (req, res) {
-  res.json('POST request');
+  res.send('create request to homepage');
+});
+
+router.patch('/', function (req, res) {
+  res.send('UPDATE request to homepage');
+});
+
+router.delete('/', function (req, res) {
+  res.send('DELETE request to homepage');
 });
 
 module.exports = router;
