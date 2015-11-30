a150f4bf7d0707213c67ab243f9bdaaaae61bb99
LaurisaNeuwirth <uberlaurisa@gmail.com>
2015-11-30 17:42:34 -0500

    LCN/ BAS/ KM Create test to create poll

---
+++ scripts/create_poll.js
@@ -0,0 +1,12 @@
+db.polls.insert([
+  {
+    title: 'Test 1',
+    choices: ['Choice1', 'Choice2','Choice3','Choice4','Choice5'],
+    votes: []
+  },
+  {
+    title: 'Test 2',
+    choices: ['ChoiceA', 'ChoiceB','ChoiceC','ChoiceD','ChoiceE'],
+    votes: []
+  }
+]);
