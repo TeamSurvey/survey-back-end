179d679875569186d6b9d3a43ffc6add0175e9f2
Ben <benjamin.a.spencer@gmail.com>
2015-11-30 16:28:36 -0500

    BAS, LCN -- Creates routes for polls
      maybe solved a merge conflict?

--- routes/polls.js
+++ routes/polls.js
@@ -3,54 +3,16 @@
 var express = require('express');
 var router = express.Router();
 
-var pollCtrl = //reuqire file here//
+var pollCtrl = require('../controllers/poll.js');
 
-//routing to an index action
-router.get('/', function(req, res, next) {
-  res.json({message: 'Hello, World!'});
+router.get('/', pollCtrl.index);
 
-// var pollCtrl = require('../controllers/poll');
+router.post('/', pollCtrl.create);
 
-//routing to an index action
-router.get('/', function(req, res, next) {
-  Poll.find().exec();
+router.get('/:id', pollCtrl.read);
 
-  next();
-});
+router.put('/:id', pollCtrl.update);
 
-//routing to a show action
-router.get('/:id', function(req, res, next) {
-
-  res.json({message: 'stuff'});
-
-  var poll_id = req.params.id;
-
-  next();
-});
-
-router.get('/', function(req, res) {
-  res.json(res.locals.polls);
-  res.status(200);
-});
-
-
-
-
-
-router.post('/', function (req, res) {
-  res.json('POST request');
-});
-
-router.post('/', function (req, res) {
-  res.send('create request to homepage');
-});
-
-router.patch('/', function (req, res) {
-  res.send('UPDATE request to homepage');
-});
-
-router.delete('/', function (req, res) {
-  res.send('DELETE request to homepage');
-});
+router.delete('/:id', pollCtrl.destroy);
 
 module.exports = router;
