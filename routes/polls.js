17863b592cb66b73cad8e2a45f9eb4f189210fa4
Ben <benjamin.a.spencer@gmail.com>
2015-11-30 11:51:30 -0500

    add various CRUD templates. Nothing set in stone yet.

--- routes/polls.js
+++ routes/polls.js
@@ -2,15 +2,30 @@
 
 var express = require('express');
 var router = express.Router();
+var pollCtrl = //reuqire file here//
 
 //routing to an index action
 router.get('/', function(req, res, next) {
   res.json({message: 'Hello, World!'});
+  next();
 });
 
 //routing to a show action
 router.get('/:id', function(req, res, next) {
   res.json({message: 'stuff'});
+  next();
+});
+
+router.get('/', function(req, res) {
+  res.json(res.locals.polls);
+  res.status(200);
+});
+
+
+
+
+router.post('/', function (req, res) {
+  res.json('POST request');
 });
 
 module.exports = router;
