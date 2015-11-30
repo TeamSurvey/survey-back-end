70e927305530c9ab5214249528f79e62d132f34c
Ben <benjamin.a.spencer@gmail.com>
2015-11-30 11:04:25 -0500

    requires the polls routes file in app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var passport = require('./lib/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var polls = require('./routes/polls');
 
 var app = express();
 
@@ -52,6 +53,7 @@ app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/polls', polls);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
