e22ce9067dea727c706d4a9e8dd44c41fa51b975
LaurisaNeuwirth <uberlaurisa@gmail.com>
2015-11-30 13:51:33 -0500

    LN - Add new Auth Controller via Saad Slacked to the whole class

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// new auth controller from Saad 11/30
+
 var passport = require('passport');
 var User = require('../models').model('User');
 
@@ -15,10 +17,10 @@ module.exports = {
         }
     },
     login : {
-        post : passport.authenticate('local', {
-            successRedirect : '/',
-            failureRedirect : '/'
-        })
+        post : passport.authenticate('local'),
+        all : function(req, res) {
+            res.sendStatus(200);
+        }
     },
     logout : {
         all : function(req, res, next) {
@@ -70,7 +72,7 @@ module.exports = {
             pUser.then(function(user) {
                 return user.setPassword(req.body.password);
             }).then(function() {
-                res.sendStatus(201);
+                res.sendStatus(200);
             }).catch(function(err) {
                 next(err);
             });
