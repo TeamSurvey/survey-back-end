5a85b3620478448e0a8d02cb14ec6d9b0adfa8a7
LaurisaNeuwirth <uberlaurisa@gmail.com>
2015-11-30 14:54:18 -0500

    LCN - Initial code for the poll controller testing after this local commit

---
+++ controllers/poll.js
@@ -0,0 +1,62 @@
+
+'use strict';
+
+var mongoose = require('mongoose');
+var Poll = require('../models/poll');
+mongoose.Promise = global.Promise;
+module.exports = mongoose.connection;
+
+var done = function() {
+  db.close();
+};
+
+var index = function() {
+  Poll.find().exec().then(function(poll) {
+    polls.forEach(function(poll) {
+      console.log(poll.toJSON());
+    });
+  }).catch(console.error).then(done);
+};
+
+var read = function (req, res, next) {
+  Poll.find({"_id": req.params.id}).exec()
+  .then(function(poll){
+    res.json(poll);
+  }).catch(function(error){
+    next(error);
+  });
+};
+
+var create = function (title, choices, owner_id) {
+  Poll.create({
+    'title': title,
+    'choices.option01': choices.option01,
+    'choices.option02': choices.option02,
+    'choices.option03': choices.option03,
+    'choices.option04': choices.option04,
+    'choices.option05': choices.option05,
+    'owner_id': owner_id
+  });
+};
+
+var update = function(id, field, value) {
+  // use this option
+  var modify = {};
+  modify[field] = value;
+  Poll.findByIdAndUpdate(id, { $set: modify }, { new: true }).exec().then(function(poll) {
+    console.log(poll.toJSON());
+  }).catch(console.error
+  ).then(done);
+};
+
+var destroy = function (id) {
+  Poll.findById(id).exec().then(function(poll) {
+    return poll.remove();
+  }).catch(console.error).then(done);
+};
+
+module.exports = {
+  index,
+  read,
+  destroy
+};
