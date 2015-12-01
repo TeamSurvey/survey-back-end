f708f5bdd58dde264ea03cabe35e967794c632e9
Ben <benjamin.a.spencer@gmail.com>
2015-12-01 12:53:35 -0500

    BAS - Update functionality worked when I updated the title value in scripts/tests.js

--- scripts/tests.js
+++ scripts/tests.js
@@ -18,6 +18,34 @@ var create = function (title, options, owner_id) {
   }).catch(console.error).then(done);
 };
 
+var read = function (req, res, next) {
+  Poll.find({"_id": req.params.id}).exec()
+  .then(function(poll){
+    res.json(poll);
+  }).catch(function(error){
+    next(error);
+  }).then(done);
+};
+
+// var update = function(id, field, value) {
+//   var modify = {};
+//   modify[field] = value;
+//   Poll.findByIdAndUpdate(id, { $set: modify }, { new: true }).exec().then(function(poll) {
+//     console.log(poll.toJSON());
+//   }).catch(console.error
+//   ).then(done);
+// };
+
+var update = function(id, field, value) {
+  Poll.findById(id).exec().then(function(poll) {
+    poll[field] = value; //this line breaks for given_name and surname due to nested schema
+    //don't nest the fields in your schema (for now)!!
+    return poll.save();
+  }).then(function(poll) {
+    console.log(poll.toJSON());
+  }).catch(console.error).then(done);
+};
+
 db.once('open', function() {
   var command = process.argv[2];
   var field, id;
@@ -34,22 +62,22 @@ db.once('open', function() {
       done();
     }
     break;
-    // case 'r':
-    //   field = process.argv[3];
-    //   var criterion = process.argv[4];
-    //   if(!criterion) {
-    //     console.log('usage: r <field> <criterion');
-    //     done();
-    //   }else {
-    //     read(field, criterion);
-    //   }
-    // break;
-    // case 'u':
-    //   id = process.argv[3];
-    //   field = process.argv[4];
-    //   var value = process.argv[5];
-    //   update(id, field, value);
-    // break;
+    case 'r':
+      id = process.argv[3];
+      var criterion = process.argv[4];
+      if(!criterion) {
+        console.log('usage: r <field> <criterion');
+        done();
+      }else {
+        read(field, criterion);
+      }
+    break;
+    case 'u':
+      id = process.argv[3];
+      field = process.argv[4];
+      var value = process.argv[5];
+      update(id, field, value);
+    break;
     // case 'd':
     //   id = process.argv[3];
     //   destroy(id);
