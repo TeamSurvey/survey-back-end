7a54139886ff6d204f00acd8282e68c006508080
Ben <benjamin.a.spencer@gmail.com>
2015-12-01 20:04:27 -0500

    BAS, LCN -  adds mongoose.Promise = global.Promise

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var mongoose = require('mongoose');
-mongoose.Promise = Promise;
+mongoose.Promise = global.Promise;
 
 mongoose.model('User', require('./User'));
 
 mongoose.connect("mongodb://localhost/survey");
 
-module.exports = mongoose;
+module.exports = mongoose.connection;
