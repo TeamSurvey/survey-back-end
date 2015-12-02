02bedce6416ea75710cc29d361133c49042a7353
Ben <benjamin.a.spencer@gmail.com>
2015-12-02 16:24:54 -0500

    BAS, LCN - adds HARDCODED create functionality only. Need to make sure FE/BE functionality works

--- controllers/pollAnswer.js
+++ controllers/pollAnswer.js
@@ -1,27 +1,32 @@
 'use strict';
 
+var util = require('util');
 var pollAnswer = require('../models/pollAnswer');
+var Poll = require('../models/poll');
 
 var done = function() {
   db.close();
 };
 
 var read = function (req, res, next) {
-  pollAnswer.find({"_id": req.body.pollID}).exec()
+  console.log("pollAnswer read params id is = ", req.params.id);
+  pollAnswer.find({"_id": req.body.id}).exec()
   .then(function(pollAnswer){
-    var agg = pollAnswer.aggregate({$match: {"pollID": }})
+    var agg = pollAnswer.aggregate({ $match: {"pollID": "565f0a753cc192af11e897ba"}}, { $group: { "_id": "$answer", "count": { $sum: 1 } } });
     res.json(agg);
+    console.log("aggregation is: " + agg);
   }).catch(function(error){
     next(error);
   }).then(done);
 };
 
-var create = function (pollID, answer) {
+var create = function (req, res, next) {
+  console.log("here are the pollAnswer create params " + util.inspect(req.body));
   pollAnswer.create({
-    'pollID': pollID,
-    'answer': answer
+    'pollID': req.body.currentPollId, //CHANGE THIS. THE VAL WILL FROM FROM FRONT END HTML DATA-ATTR
+    'answer': req.body.answer
   }).then(function(pollAnswer) {
-    console.log(pollAnswer);
+    res.json(pollAnswer);
   }).catch(console.error).then(done);;
 };
 
