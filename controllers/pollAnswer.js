1a49a79d1ad270733fe8e3b476323addfee49eca
Ben <benjamin.a.spencer@gmail.com>
2015-12-02 21:08:31 -0500

    BAS, LCN - Working on read/aggregate functionality in pollAnswer controller

--- controllers/pollAnswer.js
+++ controllers/pollAnswer.js
@@ -2,19 +2,42 @@
 
 var util = require('util');
 var pollAnswer = require('../models/pollAnswer');
-var Poll = require('../models/poll');
+// var Poll = require('../models/poll');
 
 var done = function() {
   db.close();
 };
 
+// var read = function (req, res, next) {
+//   console.log("pollAnswer read params id is = ", req.params.id);
+//   pollAnswer.find({"_id": req.body.id}).exec()
+//   .then(function(pollAnswer){
+//     var agg = pollAnswer.aggregate({ $match: {"pollID": "565f0a753cc192af11e897ba"}}, { $group: { "_id": "$answer", "count": { $sum: 1 } } });
+//     res.json(agg);
+//     console.log("aggregation is: " + agg);
+//   }).catch(function(error){
+//     next(error);
+//   }).then(done);
+// };
+
 var read = function (req, res, next) {
-  console.log("pollAnswer read params id is = ", req.params.id);
-  pollAnswer.find({"_id": req.body.id}).exec()
-  .then(function(pollAnswer){
-    var agg = pollAnswer.aggregate({ $match: {"pollID": "565f0a753cc192af11e897ba"}}, { $group: { "_id": "$answer", "count": { $sum: 1 } } });
-    res.json(agg);
-    console.log("aggregation is: " + agg);
+  var pollID = req.params.pollID;
+  console.log("HERE IS THE POLLID " +pollID);
+  pollAnswer.find({"pollID": req.params.pollID}).exec()
+  .then(function(pollAnswers){
+    console.log("pollAnswer read params id is = ", req.params.pollID);
+    var agg = pollAnswers.forEach(function(answer){
+      console.log("answer is: " + answer);
+      var choiceCount = {};
+      // if(!choiceCount.answer) {
+      //   choiceCount.answer = 1;
+      //   // console.log("in if choice:" + choiceCount);
+      // } else {
+      //   choiceCount.answer += 1;
+      //   // console.log("in else choice:" + choiceCount);
+      // }
+    });
+    // console.log("aggregation is: " + agg);
   }).catch(function(error){
     next(error);
   }).then(done);
